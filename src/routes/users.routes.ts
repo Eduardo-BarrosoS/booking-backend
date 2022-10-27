35c26946dd44725612cd7e49b7176fcff6e22578
Eduardo Barroso <barrosoeduardo64@gmail.com>
2022-10-27 01:10:02 -0300

    ...

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,14 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, RequestHandler, Response } from "express";
 import { deleteUser, getAllUser, getUser, updateUser } from "../controllers/user";
+import { verifyToken } from "../utils/verifyToken";
 
 export const usersRoute = express.Router()
 
-usersRoute.get("/checkauthentication", (req: Request, res: Response, next: NextFunction) => {
+usersRoute.get("/checkauthentication", verifyToken as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello user, you are logged in")
+})
+
+usersRoute.get("/checkuser/:id", (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello user, you are logged in")
 })
 
